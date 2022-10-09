f6b8d51ca169e98eeeebde02d22adb33975e60be
Filip Dujmusic <filip@ampnet.io>
2022-10-08 19:06:00 -0500

    test

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,74 @@
 import { PopulatedTransaction } from "ethers";
 import { Web3Storage } from "web3.storage";
 
-export async function publishTransaction(tx: PopulatedTransaction): Promise<string> {
-    console.log("tx to string", tx.toString());
-    const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweDY5YzY4M0NlMGJGZUM4REY4MDlEN0EyMmM0NTY2NTRlNjA2MzM3Y0EiLCJpc3MiOiJ3ZWIzLXN0b3JhZ2UiLCJpYXQiOjE2NjUyNDYxMzI5ODIsIm5hbWUiOiJuZXBodyJ9.zZ_2CkLtfdHCpfURlGxP6cKZamCH-EVPg99MaNNIw_0";
-    const storage = new Web3Storage({ token });
-    const file = new File(['hello world'], 'hello.txt', { type: 'text/plain' });
-    return storage.put([file]);
+
+const uuid = require('uuid');
+
+const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweDY5YzY4M0NlMGJGZUM4REY4MDlEN0EyMmM0NTY2NTRlNjA2MzM3Y0EiLCJpc3MiOiJ3ZWIzLXN0b3JhZ2UiLCJpYXQiOjE2NjUyNDYxMzI5ODIsIm5hbWUiOiJuZXBodyJ9.zZ_2CkLtfdHCpfURlGxP6cKZamCH-EVPg99MaNNIw_0";
+const storage = new Web3Storage({ token });
+
+
+interface CreateTransactionRequest {
+    tx: PopulatedTransaction;
+    chainId: number;
 }
+
+interface TransactionRequest {
+    tx: PopulatedTransaction;
+    requestId: string;
+}
+
+// interface TransactionRequestWithStatus {
+//     tx: PopulatedTransaction;
+//     requestId: string;
+//     status: string;
+// }
+
+export async function publishTransactionFlow(
+    transactions: CreateTransactionRequest[]
+): Promise<string> {
+
+    
+    const txFlow = JSON.stringify({
+        transactions: transactions.map(it => {
+            it.tx.chainId = it.chainId;
+            return {
+                tx: it.tx,
+                requestId: uuid.v4()
+            };
+        })
+    });
+
+    const file = new File( [txFlow], 'flow.json', { type: 'application/json'} );
+    return storage.put([file]);
+    // return storage.put([ new File([txFlow], 'flow.json', { type: 'text/plain' }) ]);
+    // fs.writeFileSync('./transaction.json', txFlow);
+    // const files = await getFilesFromPath('./transaction.json');
+    // return storage.put(files);
+}
+
+export async function getTransactionFlow(
+    cid: string
+): Promise<TransactionRequest[]> {
+    const response = await storage.get(cid)
+    if (!response) { Promise.reject(`NephwSDK:: Transaction with cid ${cid} not found.`); }
+    const files = await response!.files();
+    if (!files) { Promise.reject(`NephwSDK:: Transaction with cid ${cid} contains no files.`); }
+    const file = files[0];
+    const data = await file.text();
+    
+    const transactions = JSON.parse(data) as TransactionRequest[];
+    // const transactionsWithStatuses = transactions.map(t => {
+    //     return 
+    // });
+
+    return transactions;
+}
+
+// async function withStatus(txRequest: TransactionRequest): Promise<TransactionRequestWithStatus> {
+//     fs.writeFileSync('./transaction.json', JSON.stringify({
+//         ...txRequest,
+//         status: 'COMPLETED'
+//     }));
+
+// }
--- test/sdk.test.ts
+++ test/sdk.test.ts
@@ -9,15 +9,30 @@ describe('NEPHW SDK', function () {
   ];
 
   it('sdk functions', async () => {
-
     const ERC20Contract = new ethers.Contract("0xdAC17F958D2ee523a2206206994597C13D831ec7", erc20abi, provider);
-    const transaction = await ERC20Contract.populateTransaction.transfer(
+    const transaction1 = await ERC20Contract.populateTransaction.transfer(
       "0x4Ed918C7800F5dc34d2C774f6EA5fbd15f1a94a7",
       100000
     );
+    const transaction2 = await ERC20Contract.populateTransaction.transfer(
+      "0x4Ed918C7800F5dc34d2C774f6EA5fbd15f1a94a7",
+      200000
+    );
     
-    const transactionCid = await NephwSDK.publishTransaction(transaction);
-    console.log('transaction cid', transactionCid);
+    const transactionFlowCid = await NephwSDK.publishTransactionFlow([
+      {
+        tx: transaction1,
+        chainId: 1
+      },
+      {
+        tx: transaction2,
+        chainId: 5
+      }
+    ]);
+    console.log('transaction cid', transactionFlowCid);
+
+    const fetchedTx = await NephwSDK.getTransactionFlow(transactionFlowCid);
+    console.dir(fetchedTx, { depth: null });
 
   }).timeout(600000);
 
